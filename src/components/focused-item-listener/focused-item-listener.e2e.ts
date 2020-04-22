041d14d19949d6a7cb512cf7d1629327ff92cf5d
Francisco Javier Estrada <javier.estrada@autentia.com>
2020-04-22 13:48:24 +0200

    More testing
    KeyboardNavigable
    FocusedItemListener

--- src/components/focused-item-listener/focused-item-listener.e2e.ts
+++ src/components/focused-item-listener/focused-item-listener.e2e.ts
@@ -1,5 +1,5 @@
 import { newE2EPage } from '@stencil/core/testing';
-import { ItemPosition, FocusedItemHandler } from '../../abstraction/FocusedItemHandler';
+import { ItemPosition } from '../../abstraction/FocusedItemHandler';
 
 describe('focused-item-listener', () => {
   it('renders', async () => {
@@ -11,20 +11,27 @@ describe('focused-item-listener', () => {
   });
 
   it('should notify to handler when listens a focusedItem event', async () => {
-    const page = await newE2EPage();
+    const mockNotify = jest.fn();
     const position: ItemPosition = { row: 3, column: 1 };
-    const handler: FocusedItemHandler = {
-      notifyFocusedItem: jest.fn()
-    };
+    const page = await newE2EPage();
     await page.setContent('<focused-item-listener><focusable-item></focusable-item></focused-item-listener>');
+    await page.exposeFunction("notifyFocusedItem", mockNotify);
+
     const listener = await page.find('focused-item-listener');
     const focusable = await page.find("focusable-item");
 
-    focusable.setProperty("position", position);
-    listener.setProperty("handler", handler);
+    await focusable.setProperty("position", position);
+    /* await listener.setProperty("handler", handler);
+    * It doesn't work because Stencil Testing setProperty removes every function inside the object.
+    */
+    await page.$eval("focused-item-listener", (Element: any) => {
+      Element.handler = { notifyFocusedItem: this.notifyFocusedItem }
+    });
     await page.waitForChanges();
+
+    console.debug("Handler: ", await listener.getProperty("handler"));
     await focusable.focus();
 
-    expect(handler.notifyFocusedItem).toHaveBeenCalledWith(position);
+    expect(mockNotify).toHaveBeenCalledWith(position);
   });
 });
--- src/components/keyboard-navigable/keyboard-navigable.e2e.ts
+++ src/components/keyboard-navigable/keyboard-navigable.e2e.ts
@@ -16,8 +16,52 @@ describe('keyboard-navigable', () => {
     await page.setContent('<keyboard-navigable></keyboard-navigable>');
     const element = await page.find('keyboard-navigable');
     const spyEvent = await element.spyOnEvent("upArrow");
-
+    // The element must be focusable in order to press a key inside.
+    element.tabIndex = -1;
+    await page.waitForChanges();
     await element.press(KeyCodes.UP);
+
+    expect(spyEvent).toHaveReceivedEventTimes(1);
+  });
+
+  it("should notify down arrow pressed", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<keyboard-navigable></keyboard-navigable>');
+    const element = await page.find('keyboard-navigable');
+    const spyEvent = await element.spyOnEvent("downArrow");
+    // The element must be focusable in order to press a key inside.
+    element.tabIndex = -1;
+    await page.waitForChanges();
+    await element.press(KeyCodes.DOWN);
+
+    expect(spyEvent).toHaveReceivedEventTimes(1);
+  });
+
+  it("should notify left arrow pressed", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<keyboard-navigable></keyboard-navigable>');
+    const element = await page.find('keyboard-navigable');
+    const spyEvent = await element.spyOnEvent("leftArrow");
+    // The element must be focusable in order to press a key inside.
+    element.tabIndex = -1;
+    await page.waitForChanges();
+    await element.press(KeyCodes.LEFT);
+
+    expect(spyEvent).toHaveReceivedEventTimes(1);
+  });
+
+  it("should notify right arrow pressed", async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<keyboard-navigable></keyboard-navigable>');
+    const element = await page.find('keyboard-navigable');
+    const spyEvent = await element.spyOnEvent("rightArrow");
+    // The element must be focusable in order to press a key inside.
+    element.tabIndex = -1;
+    await page.waitForChanges();
+    await element.press(KeyCodes.RIGHT);
 
     expect(spyEvent).toHaveReceivedEventTimes(1);
   });
